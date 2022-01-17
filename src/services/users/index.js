d648124da0e7faa768135087f95f7b29f4428510
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-01-17 15:44:51 +0100

    Update index.js

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -52,7 +52,7 @@ userRouter.get("/googleRedirect", passport.authenticate("google"), async (req, r
    const decodedToken = await verifyJWT(user.tokens.accessToken)
    console.log("decodedToken._id",decodedToken._id)
      res.redirect(frontendUrl +`/showcasepre/${decodedToken._id}/${user.tokens.accessToken}` )
-        
+        console.log(res)
     } catch (error) {
         next(error) 
 
